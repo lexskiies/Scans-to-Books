be8fdcf248afcb3ee827c40d13745c3e6338a448
lexskiies <alexis.cheyy@gmail.com>
2025-08-03 11:40:52 +0200

    Mise à jour de PyQT5 vers PyQt6

--- src/scans_to_books/main.py
+++ src/scans_to_books/main.py
@@ -1,14 +1,14 @@
 # src/scans_to_books/main.py
 
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
 from scans_to_books.ui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
--- src/scans_to_books/ui/main_window.py
+++ src/scans_to_books/ui/main_window.py
@@ -1,26 +1,39 @@
 # src/scans_to_books/ui/main_window.py
 
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget, QLineEdit
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("scans_to_books")
-        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle("Scans to Books")
+        self.setGeometry(100, 100, 800, 600)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout()
 
-        self.label = QLabel("Bonjour, monde!")
-        self.layout.addWidget(self.label)
 
-        self.button = QPushButton("Cliquez-moi!")
-        self.button.clicked.connect(self.on_button_click)
-        self.layout.addWidget(self.button)
+        # Selection des chapitres
+        self.label = QLabel("Range de chapitres: ")
+        self.chapter_input_1 = QLineEdit()
+        self.chapter_input_2 = QLineEdit()
+        self.chapter_input_1.setPlaceholderText("Ex: 1")
+        self.chapter_input_2.setPlaceholderText("Ex: 10")
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.chapter_input_1)
+        self.layout.addWidget(self.chapter_input_2)
+
+        # Validation des entrées
+        self.validate_button = QPushButton("Valider")
+        self.validate_button.clicked.connect(self.on_validate)
+        self.layout.addWidget(self.validate_button)
+
 
         self.central_widget.setLayout(self.layout)
 
-    def on_button_click(self):
-        self.label.setText("Bouton cliqué!")
+    def on_validate(self):
+        chapter_start = self.chapter_input_1.text()
+        chapter_end = self.chapter_input_2.text()
+        
+        print(f"Chapitres sélectionnés: {chapter_start} à {chapter_end}")
